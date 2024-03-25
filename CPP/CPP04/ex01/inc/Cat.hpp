8b830828816443063e9f832f3438b6558fe92183
Zahhask <tomasgaspar95@gmail.com>
2024-03-25 17:02:15 +0000

    ex01 done

---
+++ CPP/CPP04/ex01/inc/Cat.hpp
@@ -0,0 +1,25 @@
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Cat: public Animal{
+public:
+	Cat();
+	~Cat();
+	Cat(const Cat &);
+
+	Cat& operator=(const Cat &rhs);
+
+
+	void makeSound() const;
+	std::string getType() const;
+
+private:
+	Brain* brain; //falta dar start a isto
+};
+
+
+
+#endif
