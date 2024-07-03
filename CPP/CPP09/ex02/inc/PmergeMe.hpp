00ab0cbc9e3ed6781ac6dd5e1593bbb11c67691f
Zahhask <tomasgaspar95@gmail.com>
2024-07-03 16:53:49 +0100

    Beginning of ex02

---
+++ CPP/CPP09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,26 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+#include <deque>
+#include <ctime>
+
+class PmergeMe
+{
+private:
+	std::vector<int> vec;
+	std::deque<int> deq;
+public:
+	PmergeMe();
+	~PmergeMe();
+};
+
+
+
+#endif
