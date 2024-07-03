90490b9efd4ba44c4d62571c5a6de10f73600f42
Zahhask <tomasgaspar95@gmail.com>
2024-07-03 17:01:35 +0100

    Some

---
+++ CPP/CPP09/ex02/src/main.cpp
@@ -0,0 +1,15 @@
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv){
+	if (argc < 2){
+		std::cout << "Needs arguments" << std::endl;
+		return 1;
+	}
+
+	PmergeMe FJ; //* FJ from Ford-Johnson 
+
+	
+}
+
+
+
