ea1a94fac280685c786edc80be50b83bbec9ac8d
Zahhask <tomasgaspar95@gmail.com>
2024-07-05 17:59:54 +0100

    forDeque done, i hope

--- CPP/CPP09/ex02/src/PmergeMe.cpp
+++ CPP/CPP09/ex02/src/PmergeMe.cpp
@@ -21,7 +21,7 @@ void PmergeMe::parse(char **arg){
 	//* START TIMER HERE
 	forVector();
 	//* START TIMER HERE
-	//forDeque();
+	forDeque();
 	
 }
 
@@ -152,7 +152,123 @@ void PmergeMe::forVector(){
 }
 
 
+void PmergeMe::forDeque(){
+	int count = 0;
+	//* Make pairwise comparisons of [n/2] disjoint pairs of elements. (If n is odd, leave one element out.)
+	for (size_t i = 0; i < vec.size() - 1; i += 2){
+		if (vec[i] > vec[i + 1]){
+			std::swap(vec[i], vec[i + 1]);
+			count++;
+		}
+	}
+	
+	//debug("ORIGINAL AFTER STEP 1", vec);
 
+
+	//* Sort the pairs by their larger number
+	for (size_t i = 3; i <= vec.size() - 1; i += 2){
+		for (size_t d = 1; i < vec.size(); d += 2){
+			if (d == i)
+				break;
+			if (vec[i] < vec[d]){
+				count++;
+				std::swap(vec[i - 1], vec[d - 1]);
+				std::swap(vec[i], vec[d]);
+			}	
+		}
+	}
+
+	//debug("ORIGINAL AFTER STEP 2", vec);
+
+
+	//* CREATING MAIN CHAIN AND PEND
+	std::deque<int> chain;
+	std::deque<int> pend;
+
+	chain.push_back(vec[0]);
+	for (size_t i = 1; i <= vec.size() - 1; i += 2){
+		chain.push_back(vec[i]);
+		if (i > 1)
+			pend.push_back(vec[i - 1]);
+	}
+	int haslonely = vec.size() % 2;
+	if (haslonely){
+		pend.push_back(vec[vec.size() - 1]);
+	}
+
+	//debug("MAIN CHAIN: ", chain);
+	//debug("PEND: ", pend);
+
+
+
+
+	std::deque<int> chainOriginal = chain;
+
+	//* ADD THE PEND TO THE MAIN CHAIN
+	int chainSize = chain.size();
+	int pendSize = pend.size();
+
+	for (int b = 0; b < chainSize / 2 - 1; b += 2){
+		int b2 = b + 1;
+
+		//* FIND INDEX ON CHAIN
+		std::deque<int>::const_iterator it = std::find(chain.begin(), chain.end(), chainOriginal[b2 + 2]);
+		int index = it - chain.begin();
+
+
+		for (int a = 0; a < index; a++){
+			if (pend[b2] < chain[a]){
+				count++;
+				chain.insert(chain.begin() + a, pend[b2]);
+				break;
+			}
+			else if (a == index - 1 && pend[b2] > chain[a]){
+				count++;
+				chain.insert(chain.begin() + a + 1, pend[b2]);
+				break;
+			}
+		}
+		it = std::find(chain.begin(), chain.end(), chainOriginal[b + 2]);
+		index = it - chain.begin();
+		for (int a = 0; a < index; a++){
+			if (pend[b] < chain[a]){
+				count++;
+				chain.insert(chain.begin() + a, pend[b]);
+				break;
+			}
+			else if (a == index - 1 && pend[b] > chain[a]){
+				count++;
+				chain.insert(chain.begin() + a + 1, pend[b]);
+				break;
+			}
+		}
+	}
+
+	//debug("CHAIN FIRST HALF", vec);
+	
+
+
+	for (int b = pendSize; b > chainSize / 2 - 1; b--){
+		int b2 = b - 1;
+		for (size_t a = 0; a < chain.size(); a++){
+			if (pend[b2] < chain[a]){
+				count++;
+				chain.insert(chain.begin() + a, pend[b2]);
+				break;
+			}
+			else if (a == chain.size() && pend[b2] > chain[a]){
+				count++;
+				chain.insert(chain.begin() + a + 2, pend[b2]);
+			}
+		}
+	}
+
+	//debug("CHAIN SECOND HALF", vec);
+	//std::cout << "COUNT: " << count << std::endl;
+
+	deq = chain;
+
+}
 
 
 
